0bc8b2fc0135ff27c3f430926ec5962a4e808188
aaa147147 <1397117005@qq.com>
2025-04-09 19:44:31 +0800

    修改get_ini_value

--- src/iniHandle.py
+++ src/iniHandle.py
@@ -1,13 +1,18 @@
-
 import configparser
 
+def get_ini_value(section, option):
 
-def get_ini_value(section,option):
+    section = str(section)
+    option = str(option)
+    # 去除 section 和 option 的前后空格
+    section = section.strip()
+    option = option.strip()
+
     ini = configparser.ConfigParser()
-    ini.read('./rfCKR.ini',encoding='utf-8')
-    return ini.get(section,option)
-
-
-
-
+    file_path = './rfCKR.ini'
     
+    # 读取文件
+    ini.read(file_path, encoding='utf-8')
+    
+    # 获取值
+    return ini.get(section, option)
